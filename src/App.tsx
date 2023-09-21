df7ac485c88feea30622c2de629244f1bef79f1c
ateals <ateals@icloud.com>
2023-09-21 20:04:33 +0900

    feat. 기초 세팅

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function App() {
+    return (
+        <>
+            <Teal>
+                <h1>hello world</h1>
+            </Teal>
+        </>
+    );
+}
+
+const Teal = styled.div`
+    color: teal;
+`;
+
+export default App;
